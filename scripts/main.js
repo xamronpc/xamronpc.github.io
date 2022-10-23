5f39703b136d70cb090f228eb5330f630acb00f9
xamronpc <76739817+xamronpc@users.noreply.github.com>
2022-10-23 23:22:36 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,36 +2,11 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/xam-background.png') {
-      myImage.setAttribute('src','images/xam-background2.png');
+    if(mySrc === 'https://kabayanika.com/wp-content/uploads/2022/10/xam-background.png') {
+      myImage.setAttribute('src','https://kabayanika.com/wp-content/uploads/2022/03/WhatsApp-Image-2022-03-08-at-6.46.33-PM.jpeg');
     } else {
-      myImage.setAttribute('src','images/xam-background.png');
+      myImage.setAttribute('src','https://kabayanika.com/wp-content/uploads/2022/03/5B2121C2-F239-4CD6-BF29-202248FD2527.jpeg');
     }
-}
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Welcome ' + myName;
-  }
-}
-
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Welcome ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
 }
 
 var coll = document.getElementsByClassName("collapsible");
@@ -41,29 +16,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
-    }
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
   });
 }
-
-function openPage(pageName,elmnt,color) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-  document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-
